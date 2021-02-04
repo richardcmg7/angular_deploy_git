744eaa535d5240b6d0a612c5e381842720a9d202
= <richardcmg7@gmail.com>
2021-02-04 15:23:23 -0500

    try 2

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
-path = require("path");
-const express = require("express");
-const app = express();
-app.use(express.static(__dirname + '/my-app'));
-app.get('/*', function(req,res){
-	res.sendFile(path.join(__dirname, '/my-app', 'index.html'))
-	);
-// Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT 11 8080);
+const express = require('express');
+const ngApp = express();
+ngApp.use(express.static('./dist/my-app'));
+ngApp.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/my-app/'})});
+  ngApp.listen(process.env.PORT || 8080);
